0a0a78d2269a00cab4d11b2b385cdb24fbcd9ce9
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-25 05:35:40 -0300

    fix: fixing docker image and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
-echo "\n[i] Generando cliente de base de datos..."
-npx prisma generate
-
 echo "\n[i] Aplicando modelo a la base de datos..."
 npx prisma db push
 
-echo "\n[i] Actualizando cliente de base de datos..."
+echo "\n[i] Generando cliente de base de datos..."
 npx prisma generate
 
 echo "\n[i] Iniciando el servidor..."
-node ./dist/scr/main.js
+node ./src/main.js

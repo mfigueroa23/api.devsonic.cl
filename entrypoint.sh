b0f087bcff2b014d1fdba7d105b04865c99d4ddc
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-25 11:58:44 -0300

    review: Fix CI/CD Pipe Line ok

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-echo "\n[i] Aplicando modelo a la base de datos..."
-npx prisma db push
-
-echo "\n[i] Generando cliente de base de datos..."
-npx prisma generate
-
-echo "\n[i] Iniciando el servidor..."
-node ./src/main.js

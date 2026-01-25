304f04a5ed692e251713effd21decc9495a01bb1
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-25 05:21:01 -0300

    fix: fixing image builder and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-echo -e "\n[i] Generando cliente de base de datos...\n"
+echo "\n[i] Generando cliente de base de datos..."
 npx prisma generate
 
-echo -e "\n[i] Aplicando modelo a la base de datos...\n"
+echo "\n[i] Aplicando modelo a la base de datos..."
 npx prisma db push
 
-echo -e "\n[i] Actualizando cliente de base de datos...\n"
+echo "\n[i] Actualizando cliente de base de datos..."
 npx prisma generate
 
-echo -e "\n[i] Iniciando el servidor...\n"
-node ./scr/main.js 
+echo "\n[i] Iniciando el servidor..."
+node ./dist/scr/main.js

0833a931a14b0449b6f100852bc21aa7299f5b07
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-26 12:24:08 -0300

    feature: adding notifications module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaService } from './prisma.service.js';
+import { NotificationsModule } from './notifications/notifications.module.js';
 @Module({
   controllers: [AppController],
   providers: [AppService, PrismaService],
+  imports: [NotificationsModule],
 })
 export class AppModule {}

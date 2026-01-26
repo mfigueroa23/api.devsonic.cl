d4be092c988ea78c0f0fc8f9e3f86f8c708f3e0d
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-26 14:00:49 -0300

    feat(notifications): [API-5] add base notification logic

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { NotificationPortfolio } from '../types/notifications.types.js';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  private readonly logger = new Logger(NotificationsService.name);
+  sendPortfolioNotification(message: NotificationPortfolio) {
+    this.logger.log('Procesando notificación de portafolio');
+    return {
+      name: message.name,
+      email: message.email,
+      message: message.message,
+    };
+  }
+}

62f0e09c64a87d1b73cb6fcb62e3da74e6471729
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-26 14:00:49 -0300

    feature: adding base logic

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller.js';
 import { NotificationsService } from './notifications.service.js';
+import { PrismaService } from '../prisma.service.js';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, PrismaService],
 })
 export class NotificationsModule {}

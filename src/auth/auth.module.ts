4b0a4734df22b14f5d2e4074d68919d84d4fd0e3
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-02-14 23:49:29 -0300

    feature: adding Loh In logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { UsersService } from '../users/users.service.js';
+import { CryptoUtil } from '../utils/crypto.util.js';
+import { AppService } from '../app.service.js';
+import { PrismaService } from '../prisma.service.js';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService, CryptoUtil, AppService, PrismaService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,38 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { AuthBody } from 'src/types/auth.types.js';
+import { AuthBody } from '../types/auth.types.js';
+import { UsersService } from '../users/users.service.js';
+import { CryptoUtil } from '../utils/crypto.util.js';
+import { AppService } from '../app.service.js';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private cryptoUtil: CryptoUtil,
+    private appService: AppService,
+  ) {}
   private readonly logger = new Logger(AuthService.name);
-  logIn(authBody: AuthBody) {
+  async logIn(authBody: AuthBody): Promise<boolean> {
     try {
       this.logger.log(
-        `Procesando inicio de secion, usuario: ${authBody.email}`,
+        `Procesando inicio de sesion, usuario: ${authBody.email}`,
       );
-      return true;
+      const userPassword = await this.userService.getUserPasswd(authBody.email);
+      const cryptoSecret = await this.appService.getProperty('CRYPTO_SECRET');
+      const cryptoSalt = await this.appService.getProperty('CRYPTO_SALT');
+      const userPasswdPlainText = this.cryptoUtil.decrypt(
+        userPassword,
+        cryptoSecret.value,
+        cryptoSalt.value,
+      );
+      this.logger.log('Comparando contraseñas');
+      if (authBody.password === userPasswdPlainText) {
+        this.logger.log('Credenciales validas');
+        return true;
+      } else {
+        this.logger.warn('Credenciales invalidas');
+        throw new Error('Credenciales invalidas');
+      }
     } catch (err) {
       const error = new Error(err as string);
       this.logger.error(
